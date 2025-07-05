1d895140bbf555bd0568dbc57c4ce884b5573b5c
Andrew Smith <andrew@andrewnsmith.net>
2025-07-05 14:50:19 -0400

    backup

--- lua/erastos/plugins/lsp/lspconfig.lua
+++ lua/erastos/plugins/lsp/lspconfig.lua
@@ -78,49 +78,83 @@ return {
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["yamlls"] = function()
-				lspconfig["yamlls"].setup({
-					capabilities = capabilities,
-					settings = {
-						yaml = {
-							schemas = {
-								kubernetes = "*.yaml",
-								["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-								["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-								["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
-								["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-								["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-								["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-								["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
-							},
-						},
+		lspconfig.yamlls.setup({
+			capabilities = capabilities,
+			settings = {
+				yaml = {
+					schemas = {
+						kubernetes = "*.yaml",
+						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+						["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+						["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+						["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+						["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
 					},
-				})
-			end,
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				})
-			end,
+				},
+			},
 		})
+
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					-- make the language server recognize "vim" global
+					diagnostics = {
+						globals = { "vim" },
+					},
+					completion = {
+						callSnippet = "Replace",
+					},
+				},
+			},
+		})
+
+		-- mason_lspconfig.setup_handlers({
+		-- 	-- default handler for installed servers
+		-- 	function(server_name)
+		-- 		lspconfig[server_name].setup({
+		-- 			capabilities = capabilities,
+		-- 		})
+		-- 	end,
+		-- 	["yamlls"] = function()
+		-- 		lspconfig["yamlls"].setup({
+		-- 			capabilities = capabilities,
+		-- 			settings = {
+		-- 				yaml = {
+		-- 					schemas = {
+		-- 						kubernetes = "*.yaml",
+		-- 						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+		-- 						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+		-- 						["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+		-- 						["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+		-- 						["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+		-- 						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+		-- 						["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+		-- 					},
+		-- 				},
+		-- 			},
+		-- 		})
+		-- 	end,
+		-- 	["lua_ls"] = function()
+		-- 		-- configure lua server (with special settings)
+		-- 		lspconfig["lua_ls"].setup({
+		-- 			capabilities = capabilities,
+		-- 			settings = {
+		-- 				Lua = {
+		-- 					-- make the language server recognize "vim" global
+		-- 					diagnostics = {
+		-- 						globals = { "vim" },
+		-- 					},
+		-- 					completion = {
+		-- 						callSnippet = "Replace",
+		-- 					},
+		-- 				},
+		-- 			},
+		-- 		})
+		-- 	end,
+		-- })
+		mason_lspconfig.setup({})
 	end,
 }
