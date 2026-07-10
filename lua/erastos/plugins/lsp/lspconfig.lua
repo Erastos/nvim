10fe652714db1636df5a17b361dc1ffb65dda436
Andrew Smith <andrew@andrewnsmith.net>
2026-07-10 13:46:07 -0400

    Add YAML Schema

--- lua/erastos/plugins/lsp/lspconfig.lua
+++ lua/erastos/plugins/lsp/lspconfig.lua
@@ -64,7 +64,7 @@ return {
             ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*",
             ["https://json.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
             ["https://gitlab.com/gitlab-org/gitlab-foss/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.yml",
-            ["https://gitlab.com/gitlab-org/gitlab-foss/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab/*",
+            ["https://gitlab.com/gitlab-org/gitlab-foss/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab/**/*.{yml,yaml}",
           },
         },
       },
