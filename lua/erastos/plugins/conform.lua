e9fb2ace391cc4794c221f355f90e5fa12a5056b
Andrew Smith <andrew@andrewnsmith.net>
2026-08-17 08:00:46 -0400

    Add DiableFormat command

--- lua/erastos/plugins/conform.lua
+++ lua/erastos/plugins/conform.lua
@@ -1,7 +1,7 @@
 return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  cmd = { "ConformInfo", "FormatDisable", "FormatEnable" },
   keys = {
     {
       "<leader>mp",
@@ -27,9 +27,27 @@ return {
       terraform = { "terraform_fmt" },
       tf = { "terraform_fmt" },
     },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
+    format_on_save = function(bufnr)
+      if vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
   },
+
+  config = function(_, opts)
+    require("conform").setup(opts)
+    vim.api.nvim_create_user_command("FormatDisable", function()
+      vim.b.disable_autoformat = true
+      vim.notify("Autoformat disabled for this buffer", vim.log.levels.INFO)
+    end, {
+      desc = "Disable format-on-save for the current buffer",
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.notify("Autoformat enabled for this buffer", vim.log.levels.INFO)
+    end, {
+      desc = "Re-enable format-on-save for the current buffer",
+    })
+  end,
 }
